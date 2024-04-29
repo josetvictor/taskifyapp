b16aa304f4c783255cf00edf2c541a867dc20168
Jose Victor <zevicctor2@gmail.com>
2024-04-28 22:06:31 -0300

    add task error handling

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,20 +1,43 @@
 import TaskSchema from "../schemas/tasks.js";
 
-function findAll(req, res) {
-  TaskSchema.findAll().then((result) => res.json(result));
+async function findAll(req, res) {
+  try {
+    const tasksAll = await TaskSchema.findAll();
+
+    return res.status(200).json(tasksAll);
+  } catch (error) {
+    return res.status(500).json({ message: "error in finding tasks" });
+  }
 }
 
-function findTask(req, res) {
-  TaskSchema.findByPk(req.params.id).then((result) => res.json(result));
+async function findTask(req, res) {
+  try {
+    const taskfound = await TaskSchema.findOne({ where: { id: req.params.id } });
+
+    if(!taskfound) { return res.status(404).json({ message: "Task not found" }) }
+
+    return res.status(200).json(taskfound);
+  } catch (error) {
+    return res.status(500).json({ message: "error in finding task" });
+  }
 }
 
-function addTask(req, res) {
-  TaskSchema.create({
-    user_id: req.body.user_id,
-    title: req.body.title,
-    description: req.body.description,
-    completed: req.body.completed,
-  }).then((result) => res.json(result));
+async function addTask(req, res) {
+  try {
+    const { user_id, title, description, completed } = req.body;
+
+    const taskCreated = await TaskSchema.create({
+      user_id,
+      title,
+      description,
+      completed,
+    });
+
+    return res.status(201).json({ message: "Task created successfully", taskCreated });
+    
+  } catch (error) {
+    return res.status(500).json({ message: "error in adding task" });
+  }
 }
 
 async function deleteTask(req, res) {
@@ -25,6 +48,21 @@ async function deleteTask(req, res) {
   });
 
   TaskSchema.findAll().then((result) => res.json(result));
+
+  try {
+    const {id} = req.params;
+    const task = await TaskSchema.findOne({ where: { id } });
+
+    if(!task) {
+      return res.status(404).json({ message: "Task not exists" });
+    }
+
+    await TaskSchema.destroy({ where: { id } });
+
+    return res.status(200).json({ message: "Task deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "error in deleting task" });
+  }
 }
 
 export default { findAll, findTask, addTask, deleteTask };
